4e9f825da50e45479e791202f8c81b3a6a0fbbcd
Mikolaj <mstruzykowski@gmail.com>
2024-07-18 00:34:16 +0200

    usefull coments

--- graph.hpp
+++ graph.hpp
@@ -49,6 +49,29 @@ class edgeList{
     void removeEdge(int);
     void removeVertex(std::string);
 
+    /*
+
+    CZEMU NIBY UŻYWASZ INT I STRING DO USUWANIA I POSZUKIWANIA 
+    SĄSIADUJĄCYCH??????????????
+    TO MAJĄ BYĆ OBIEKTY
+    FUCNA ISADJECTESDTO() POWINNA PRZYJMOWAĆ VERTES OBEJCT
+    ISADJEECTEDTO(VERTEX) I W PROGRAMIE ODPOWIEDNIO ITEROWAĆ PO 
+    GRAPHIE!!!
+
+    W OGÓLE TEN PLIK JEST AKTUALNY????
+
+    ONES KEFT \/
+    (to powinny być funnkcje klasy edge)
+    endVertices(Edge class e) -> return vertex connected to edge
+    opposite(vertex class v) -> returns the oter vertex beside v
+    isAdjectedToEdge(edge class e) -> check
+    isIncidentOn(vertex class c) -> check
+
+
+    Ale i tak są źle te rzeczy i nie wiem czemu nie zrobiłeś według ksiązki 
+    z podziałem na metody klas graph, edge i vertex ??      
+    */
+
 };
 
 
